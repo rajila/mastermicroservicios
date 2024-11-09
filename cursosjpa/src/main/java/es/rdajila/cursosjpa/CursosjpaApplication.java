89128359345b30c9e2f6d20e66a8ec7a4ef3b41c
rajila <rdajila@gmail.com>
2024-11-09 16:36:38 +0100

    changes: init app cursosjpa

---
+++ cursosjpa/src/main/java/es/rdajila/cursosjpa/CursosjpaApplication.java
@@ -0,0 +1,13 @@
+package es.rdajila.cursosjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosjpaApplication.class, args);
+	}
+
+}
---
+++ cursosjpa/src/test/java/es/rdajila/cursosjpa/CursosjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.rdajila.cursosjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
