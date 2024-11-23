1099d1f00294879a1fd08450f0a019724443cde1
rajila <rdajila@gmail.com>
2024-11-23 05:28:23 +0100

    init app cliente courses

---
+++ clientcourses/src/main/java/es/rdajila/clientcourses/ClientcoursesApplication.java
@@ -0,0 +1,13 @@
+package es.rdajila.clientcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientcoursesApplication.class, args);
+	}
+
+}
