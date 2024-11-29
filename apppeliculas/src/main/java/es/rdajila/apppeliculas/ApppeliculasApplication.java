aca9d0d95cdedb8859654dc0a2edfc766aa2c5f0
rajila <rdajila@gmail.com>
2024-11-29 23:46:28 +0100

    init apppeliculas

---
+++ apppeliculas/src/main/java/es/rdajila/apppeliculas/ApppeliculasApplication.java
@@ -0,0 +1,13 @@
+package es.rdajila.apppeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApppeliculasApplication.class, args);
+	}
+
+}
---
+++ apppeliculas/src/test/java/es/rdajila/apppeliculas/ApppeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.rdajila.apppeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
