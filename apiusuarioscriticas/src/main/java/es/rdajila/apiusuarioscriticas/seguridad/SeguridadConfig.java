2155068a145b1120fd848b912ef3bbdc8cfc2d7d
Ronald <rdajila@gmail.com>
2024-12-30 15:47:06 +0000

    changes: config seguridad auth login jwt

--- apiusuarioscriticas/src/main/java/es/rdajila/apiusuarioscriticas/seguridad/SeguridadConfig.java
+++ apiusuarioscriticas/src/main/java/es/rdajila/apiusuarioscriticas/seguridad/SeguridadConfig.java
@@ -1,5 +1,7 @@
 package es.rdajila.apiusuarioscriticas.seguridad;
 
+import es.rdajila.apiusuarioscriticas.AuthEntryPointJwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,16 +10,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SeguridadConfig {
+    private final AuthEntryPointJwt authEntryPoint;
+
+    @Autowired
+    public SeguridadConfig(AuthEntryPointJwt authEntryPoint) {
+        this.authEntryPoint = authEntryPoint;
+    }
 
     @SuppressWarnings({"removal" })
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/usuarios/**").permitAll()
                         .requestMatchers("/api/roles/**").permitAll()
@@ -25,8 +41,25 @@ public class SeguridadConfig {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
+                http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public  AuthenticationFilterJwt jwtAuthenticationFilter() {
+        return new AuthenticationFilterJwt();
+    }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
